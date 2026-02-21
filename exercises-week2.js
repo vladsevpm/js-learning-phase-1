72353e741db7c5530332984557dc4b8704a9762d
vladsevpm <vladsev.pm@gmail.com>
2026-02-21 18:49:02 +0200

    Exe 3

--- exercises-week2.js
+++ exercises-week2.js
@@ -25,3 +25,27 @@ console.log("\n--- Exercise 2 ---")
 console.log(title.toUpperCase());
 console.log(title.length);
 console.log(capitalizedString);
+
+//Exercises 3 
+
+//You go to the supermarket. Create variables for:
+
+const applep = 1.50 //Apple Price
+const breadp = 2.30 //Bread Price
+const milkp = 0.99 //Milk Price
+
+const applea = 3 //Amount apples
+const breada = 2 //Amount bread
+const milka = 1 //Amount milk
+
+const change = 20
+const discount = 0.1
+
+const totalcost = applea*applep+breada*breadp+milka*milkp
+const afterdiscount = totalcost - (totalcost * discount)
+const changeback = change - afterdiscount 
+
+console.log("\n--- Exercise 3 ---")
+console.log(`Total: ${totalcost}`) //Total cost
+console.log(`After 10% discount: ${afterdiscount}`) //Total cost with 10% discount 
+console.log(`Change from €20 ${changeback}`) //Change from 20 Euro
