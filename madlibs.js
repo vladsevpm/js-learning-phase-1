3a4af9f90dfd62f78af132de3595d82fd1e78be4
vladsevpm <vladsev.pm@gmail.com>
2026-02-21 17:47:18 +0200

    Add Mad Libs mini-project

---
+++ madlibs.js
@@ -0,0 +1,10 @@
+const name = "Garrett"; // a persons's name
+const adjective = "rouge"; // describing word
+const verb = "took"; // action word
+const place = "castle";
+const number = 38;
+
+const story = `One day, ${name} woke up and feeling very ${adjective}. 
+They ${verb} all the way to the ${place}, bought ${number} arrows, and never looked back.` ;
+
+console.log(story);
