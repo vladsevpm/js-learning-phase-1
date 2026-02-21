9bad04e5d7ba3208b2cf895845a745bc14f280c9
vladsevpm <vladsev.pm@gmail.com>
2026-02-21 17:53:06 +0200

    Add exercise 1

---
+++ exercises-week2.js
@@ -0,0 +1,9 @@
+//Exercises 1
+
+const name = "Vlad"
+const surname = "Severehchuk"
+const city = "Kyiv"
+
+const message = `Hi, my name is ${name} ${surname} and I live in ${city}.`
+
+console.log(message);
